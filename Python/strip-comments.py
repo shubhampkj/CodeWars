6e42bd4f2397a0dfdf028d1a9497796c5b89d370
aman <amansaxena8055@gmail.com>
2020-09-19 15:56:54 +0530

    <4-kyu> Strip Comments - Added Aman's Approach

--- Python/strip-comments.py
+++ Python/strip-comments.py
@@ -5,6 +5,43 @@ Link: https://www.codewars.com/kata/51c8e37cee245da6b40000bd
 '''
 
 ######################
+# Aman's Approach #
+######################
+
+import re
+
+def solution(string,markers):
+    
+    if '^' in markers or '-' in markers:
+        try:
+            markers.insert(markers.index("^"), '\\')
+            markers.insert(markers.index("-"), '\\')
+        except:
+            pass
+
+    mark_str = "".join(markers)
+    
+    if markers != [] and string != '':    
+        
+        if re.search(fr"\w[{mark_str}].*", string):
+            f = re.sub(fr"[{mark_str}].*", '', string)
+            return f
+        
+        elif re.search(fr"\s*[{mark_str}].*", string):
+            f = re.sub(fr".?[{mark_str}].*", '', string)
+            return f
+        
+        else:
+            return string
+            
+    elif string == "":
+        return ""
+    
+    else:
+        return string
+
+
+######################
 # Shubham's Approach #
 ######################
 
